7ffc2464b8c801c6d3f3c771d6de913dc1f6748c
skpark33 <skpark33333@gmail.com>
2024-10-25 09:47:16 +0900

    0.6.97

--- src_modify/modify.bat
+++ src_modify/modify.bat
@@ -1,104 +1,105 @@
 #
 # C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\
+# C:\Users\skpar\AppData\Local -> C:\Users\skpar\AppData\Local
 #
 
 
 REM skpark modify 3\6
-copy src_modify\r_dotted_line_border.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\r_dotted_line_border-0.1.1\lib\r_dotted_line_border.dart"
+copy src_modify\r_dotted_line_border.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\r_dotted_line_border-0.1.1\lib\r_dotted_line_border.dart"
 
 REM skpark modify 4\24
-copy src_modify\adaptive_scrollbar.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\adaptive_scrollbar-2.1.2\lib\adaptive_scrollbar.dart"
+copy src_modify\adaptive_scrollbar.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\adaptive_scrollbar-2.1.2\lib\adaptive_scrollbar.dart"
 
 REM skpark modify 7\03
-copy src_modify\neonpen.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\neonpen-2.0.5\lib\neonpen.dart"
+copy src_modify\neonpen.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\neonpen-2.0.5\lib\neonpen.dart"
 
 REM skpark modify 7\06
-copy src_modify\weather_thunder_bg.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_thunder_bg.dart"
-copy src_modify\weather_rain_snow_bg.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_rain_snow_bg.dart"
-copy src_modify\weather_night_star_bg.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_night_star_bg.dart"
-copy src_modify\weather_cloud_bg.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_cloud_bg.dart"
-copy src_modify\weather_bg.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_bg.dart"
-copy src_modify\print_utils.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\utils\print_utils.dart"
+copy src_modify\weather_thunder_bg.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_thunder_bg.dart"
+copy src_modify\weather_rain_snow_bg.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_rain_snow_bg.dart"
+copy src_modify\weather_night_star_bg.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_night_star_bg.dart"
+copy src_modify\weather_cloud_bg.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_cloud_bg.dart"
+copy src_modify\weather_bg.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\bg\weather_bg.dart"
+copy src_modify\print_utils.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\flutter_weather_bg_null_safety-1.0.0\lib\utils\print_utils.dart"
 
 REM nr.han modify 9\12
-copy src_modify\html_editor_enhanced\summernote-lite.min.jsxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\summernote-lite.min.js"
-copy src_modify\html_editor_enhanced\html_editor_widget_web.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\html_editor_widget_web.dart"
-copy src_modify\html_editor_enhanced\toolbar_widget.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\toolbar_widget.dart"
-copy src_modify\html_editor_enhanced\options.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\utils\options.dart"
+copy src_modify\html_editor_enhanced\summernote-lite.min.jsxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\summernote-lite.min.js"
+copy src_modify\html_editor_enhanced\html_editor_widget_web.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\html_editor_widget_web.dart"
+copy src_modify\html_editor_enhanced\toolbar_widget.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\toolbar_widget.dart"
+copy src_modify\html_editor_enhanced\options.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\utils\options.dart"
 
 REM nr.han modify 9\15
 REM ※ C:\Users\seon\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font 폴더에 폰트 파일을 추가해줘야함.
 REM ※ creta03의 fonts 폴더에 있는 모든 폰트 폴더를 복사 후 위에 경로에 붙여넣기 해주기
 
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Jua"
-copy src_modify\html_editor_enhanced\Jua-Regular.ttf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Jua\Jua-Regular.ttf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Macondo"
-copy src_modify\html_editor_enhanced\Macondo-Regular.ttf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Macondo\Macondo-Regular.ttf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Gothic"
-copy src_modify\html_editor_enhanced\NanumGothic-Regular.ttf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Gothic\NanumGothic-Regular.ttf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Myeongjo"
-copy src_modify\html_editor_enhanced\NanumMyeongjo-Regular.ttf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Myeongjo\NanumMyeongjo-Regular.ttf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Pen_Script"
-copy src_modify\html_editor_enhanced\NanumPenScript-Regular.ttf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Pen_Script\NanumPenScript-Regular.ttf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans_KR"
-copy src_modify\html_editor_enhanced\NotoSansKR-Regular.otf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans_KR\NotoSansKR-Regular.otf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans"
-copy src_modify\html_editor_enhanced\NotoSans-Regular.ttf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans\NotoSans-Regular.ttf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Pretendard"
-copy src_modify\html_editor_enhanced\Pretendard-Regular.otf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Pretendard\Pretendard-Regular.otf"
-mkdir "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Zocial"
-copy src_modify\html_editor_enhanced\Zocial.ttf "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Zocial\Zocial.ttf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Jua"
+copy src_modify\html_editor_enhanced\Jua-Regular.ttf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Jua\Jua-Regular.ttf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Macondo"
+copy src_modify\html_editor_enhanced\Macondo-Regular.ttf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Macondo\Macondo-Regular.ttf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Gothic"
+copy src_modify\html_editor_enhanced\NanumGothic-Regular.ttf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Gothic\NanumGothic-Regular.ttf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Myeongjo"
+copy src_modify\html_editor_enhanced\NanumMyeongjo-Regular.ttf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Myeongjo\NanumMyeongjo-Regular.ttf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Pen_Script"
+copy src_modify\html_editor_enhanced\NanumPenScript-Regular.ttf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Nanum_Pen_Script\NanumPenScript-Regular.ttf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans_KR"
+copy src_modify\html_editor_enhanced\NotoSansKR-Regular.otf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans_KR\NotoSansKR-Regular.otf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans"
+copy src_modify\html_editor_enhanced\NotoSans-Regular.ttf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Noto_Sans\NotoSans-Regular.ttf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Pretendard"
+copy src_modify\html_editor_enhanced\Pretendard-Regular.otf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Pretendard\Pretendard-Regular.otf"
+mkdir "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Zocial"
+copy src_modify\html_editor_enhanced\Zocial.ttf "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\font\Zocial\Zocial.ttf"
 
-copy src_modify\html_editor_enhanced\toolbar_widget.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\toolbar_widget.dart"
-copy src_modify\html_editor_enhanced\pubspec.yaxl "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\pubspec.yaml"
-copy src_modify\html_editor_enhanced\summernote-lite.min.cxs "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\summernote-lite.min.css"
+copy src_modify\html_editor_enhanced\toolbar_widget.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\src\widgets\toolbar_widget.dart"
+copy src_modify\html_editor_enhanced\pubspec.yaxl "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\pubspec.yaml"
+copy src_modify\html_editor_enhanced\summernote-lite.min.cxs "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\assets\summernote-lite.min.css"
 REM seventhstone 23\10\10
-copy src_modify\html_editor_enhanced\utils.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\utils\utils.dart"
+copy src_modify\html_editor_enhanced\utils.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\html_editor_enhanced-2.5.1\lib\utils\utils.dart"
 
 
 REM seventhstone 23\10\10
-copy src_modify\snowflake_model.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\snowfall-0.0.2\lib\snowfall\snowflake_model.dart"
-copy src_modify\snowfall_pubspec.yaml "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\snowfall-0.0.2\pubspec.yaml"
+copy src_modify\snowflake_model.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\snowfall-0.0.2\lib\snowfall\snowflake_model.dart"
+copy src_modify\snowfall_pubspec.yaml "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\snowfall-0.0.2\pubspec.yaml"
 
 REM skpark modify 23\10\10
-copy src_modify\CustomRadioButton.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\custom_radio_grouped_button-2.2.1\lib\CustomButtons\CustomRadioButton.dart"
+copy src_modify\CustomRadioButton.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\custom_radio_grouped_button-2.2.1\lib\CustomButtons\CustomRadioButton.dart"
 
 
 REM skpark modify 23\10\24
-copy src_modify\just_audio.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\just_audio-0.9.37\lib\just_audio.dart"
+copy src_modify\just_audio.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\just_audio-0.9.37\lib\just_audio.dart"
 
 REM seventhstone modify 23\11\03
-REM copy src_modify\appwrite_pubspec.yaxl "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\appwrite-11.0.0\pubspec.yaml"
-REM copy src_modify\dart_appwrite_pubspec.yaxl "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\dart_appwrite-10.0.0\pubspec.yaml"
-REM copy src_modify\image_network_pubspec.yaxl "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\image_network-2.5.4+1\pubspec.yaml"
-REM copy src_modify\webviewimage_pubspec.yaxl "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\webviewimage-0.0.4\pubspec.yaml"
+REM copy src_modify\appwrite_pubspec.yaxl "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\appwrite-11.0.0\pubspec.yaml"
+REM copy src_modify\dart_appwrite_pubspec.yaxl "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\dart_appwrite-10.0.0\pubspec.yaml"
+REM copy src_modify\image_network_pubspec.yaxl "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\image_network-2.5.4+1\pubspec.yaml"
+REM copy src_modify\webviewimage_pubspec.yaxl "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\webviewimage-0.0.4\pubspec.yaml"
 
 REM skpark modify 23\11\10
-REM copy src_modify\realTime_mixin.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\appwrite-11.0.0\lib\src\realTime_mixin.dart"
+REM copy src_modify\realTime_mixin.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\appwrite-11.0.0\lib\src\realTime_mixin.dart"
 
 REM skpark modify 23\11\14
-REM copy src_modify\color_picker.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\flex_color_picker-3.4.1\lib\src\color_picker.dart"
+REM copy src_modify\color_picker.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\flex_color_picker-3.4.1\lib\src\color_picker.dart"
 
 REM skpark modify 23\11\20
-copy src_modify\scroll_loop_auto_scroll.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\scroll_loop_auto_scroll-0.0.5\lib\scroll_loop_auto_scroll.dart"
+copy src_modify\scroll_loop_auto_scroll.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\scroll_loop_auto_scroll-0.0.5\lib\scroll_loop_auto_scroll.dart"
 
 REM nr.han modify 23\11\22
-REM copy src_modify\client_browser.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\appwrite-11.0.0\lib\src\client_browser.dart"
+REM copy src_modify\client_browser.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\appwrite-11.0.0\lib\src\client_browser.dart"
 
 REM skpark modify 23\12\26
-copy src_modify\tag_editor.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\material_tag_editor-0.1.2\lib\tag_editor.dart"
+copy src_modify\tag_editor.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\material_tag_editor-0.1.2\lib\tag_editor.dart"
 
 REM skpark modify 23\12\28
-copy src_modify\radio_button.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\group_radio_button-1.3.0\lib\src\radio_button.dart"
-copy src_modify\radio_group.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\group_radio_button-1.3.0\lib\src\radio_group.dart"
+copy src_modify\radio_button.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\group_radio_button-1.3.0\lib\src\radio_button.dart"
+copy src_modify\radio_group.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\group_radio_button-1.3.0\lib\src\radio_group.dart"
 
 REM skpark modify 24/05/14
-copy src_modify\dropdown_button2.daxt "$env:LOCALAPPDATA/Pub/Cache/hosted/pub.dev/dropdown_button2-1.9.4/lib/dropdown_button2.dart"
-copy src_modify\extension_navigation.daxt "$env:LOCALAPPDATA/Pub/Cache/hosted/pub.dev/get-4.6.5/lib/get_navigation/src/extension_navigation.dart"
+copy src_modify\dropdown_button2.daxt "C:\Users\skpar\AppData\Local/Pub/Cache/hosted/pub.dev/dropdown_button2-1.9.4/lib/dropdown_button2.dart"
+copy src_modify\extension_navigation.daxt "C:\Users\skpar\AppData\Local/Pub/Cache/hosted/pub.dev/get-4.6.5/lib/get_navigation/src/extension_navigation.dart"
 
 REM skpark modify 24/08/19
-copy src_modify\carousel_slider.daxt "$env:LOCALAPPDATA/Pub/Cache/hosted/pub.dev/carousel_slider-4.2.1\lib\carousel_slider.dart"
+copy src_modify\carousel_slider.daxt "C:\Users\skpar\AppData\Local/Pub/Cache/hosted/pub.dev/carousel_slider-4.2.1\lib\carousel_slider.dart"
 
 
 REM skpark modify 24/10/05
-copy src_modify\video_player.daxt "$env:LOCALAPPDATA\Pub\Cache\hosted\pub.dev\video_player-2.8.6\lib\video_player.dart" 
+copy src_modify\video_player.daxt "C:\Users\skpar\AppData\Local\Pub\Cache\hosted\pub.dev\video_player-2.8.6\lib\video_player.dart" 
